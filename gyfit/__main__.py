c4c7ac9408a54fa489fdeb8d50f0648eed3abcca
SimonChaw <chawblah@gmail.com>
2017-10-27 22:44:12 -0300

    Able to pull data from reddit

--- gyfit/__main__.py
+++ gyfit/__main__.py
@@ -1,20 +1,19 @@
-# Initialize PRAW with a custom User-Agent
-def main(args=None):
-    r = praw.Reddit('Simple comment parser from WhyCouch')
+import praw
+import time
+import logging
 
-    polite_users = set()   # to avoid duplicates
+handler = logging.StreamHandler()
+handler.setLevel(logging.DEBUG)
+logger = logging.getLogger('prawcore')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(handler)
 
-    for i in xrange(0,10):  # Run the loop 10 times
-        comments = r.get_comments('askreddit')
-        for comment in comments:
-            body = comment.body.lower()
-            if body.find("thank") != -1 or body.find("please") != -1:
-                polite_users.add(comment.author)
-        time.sleep(120)   # Sleep for 2 minutes
+r = praw.Reddit('bot1', user_agent='bot1 user agent')
 
-print "The polite users were :"
-for user in polite_users:
-    print user
+polite_users = set()   # to avoid duplicates
+# assume you have a Reddit instance bound to variable `reddit`
+subreddit = r.subreddit('redditdev')
 
-if __name__ == "__main__":
-    main()
+print(subreddit.display_name)  # Output: redditdev
+print(subreddit.title)         # Output: reddit Development
+print(subreddit.description)   # Output: A subreddit for discussion of ...
