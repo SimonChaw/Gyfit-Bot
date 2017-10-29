ee56b2889c4f74369a7fd610a0a9ae6a0ab1cd72
SimonChaw <chawblah@gmail.com>
2017-10-29 12:01:50 -0300

    Finds comments calling the bot on youtube videos that are under 1M30S

--- gyfit/__main__.py
+++ gyfit/__main__.py
@@ -1,19 +1,28 @@
+# -*- coding: utf-8 -*-
 import praw
 import time
 import logging
+import helpers
 
-handler = logging.StreamHandler()
-handler.setLevel(logging.DEBUG)
-logger = logging.getLogger('prawcore')
-logger.setLevel(logging.DEBUG)
-logger.addHandler(handler)
+#handler = logging.StreamHandler()
+#handler.setLevel(logging.DEBUG)
+#logger = logging.getLogger('prawcore')
+#logger.setLevel(logging.DEBUG)
+#logger.addHandler(handler)
 
 r = praw.Reddit('bot1', user_agent='bot1 user agent')
 
-polite_users = set()   # to avoid duplicates
-# assume you have a Reddit instance bound to variable `reddit`
-subreddit = r.subreddit('redditdev')
 
-print(subreddit.display_name)  # Output: redditdev
-print(subreddit.title)         # Output: reddit Development
-print(subreddit.description)   # Output: A subreddit for discussion of ...
+
+subreddit = r.subreddit('WinStupidPrizes')
+for submission in subreddit.stream.submissions():#get new submissions in this subreddit
+    if "youtube" in submission.url:# if the url is a youtube link
+        submission.comments.replace_more(limit=0)#Get all comments in post
+        comments = submission.comments.list()
+        for comment in comments:#Check if a user is requesting the help of Gyfit-Bot
+            if comment.body == '!Gyfit':
+                print 'Got one!' ##we found someone who wants the bots help!
+                print comment.author
+                if helpers.getVideoLength(submission.url.split('=')[1]):
+                    #its the proper length
+                    comment.reply("Help is on the way!")
---
+++ gyfit/helpers.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import json
+import urllib
+import os
+
+with open(os.path.dirname(__file__) + '/../youtube_api_key') as f:
+    api_key = f.read()
+
+def getVideoLength(video_id):
+    print video_id
+    searchUrl="https://www.googleapis.com/youtube/v3/videos?id="+video_id+"&key="+api_key+"&part=contentDetails"
+    response = urllib.urlopen(searchUrl).read()
+    data = json.loads(response)
+    all_data=data['items']
+    contentDetails=all_data[0]['contentDetails']
+    duration=contentDetails['duration']
+    return isProperLength(duration)
+
+def isProperLength(duration):
+    #Duration will look like PT6M22S or PT1H20M29S or PT20S
+    if 'H' not in duration:
+        #check for M
+        if 'M' not in duration:
+            return True
+        else:
+            #Check if M is equal to 1 and S are 30 or below
+            time=duration.split('PT')
+            MS = "".join(time).split('M')
+            M = int(MS[0])
+            S = int(MS[1].split('S')[0])
+            if M == 1 and S <= 30:
+                return True
+            else:
+                return False
+    else:
+        return False
